42a0701707ff62d39cdfcdbd6a276e254446da7b
gauss963 <gauss963@gmail.com>
2024-11-11 18:13:54 +0800

    Finish FFT

--- code/PlotWaveformFFT.f90
+++ code/PlotWaveformFFT.f90
@@ -18,14 +18,14 @@ program PlotWaveformFFT
     character(len=4) :: CodeString
     
     ! Variables for FFT
-    integer :: n_fft
-    complex, allocatable :: fft_data(:,:)
+    integer :: n_fft, n_freq
+    complex, allocatable :: fft_data(:)
     real, allocatable :: freq(:)
     real, allocatable :: amplitude(:,:)
+    real, parameter :: pi = 3.14159265359
     
     ! Variables for PGPLOT
     integer :: pgid
-    real(c_float), allocatable :: E1(:), E2(:), E3(:)
     real :: min_T, max_T, min_E, max_E
     real :: min_F, max_F, min_A, max_A
 
@@ -59,29 +59,38 @@ program PlotWaveformFFT
     end do
     
     ! Prepare for FFT
-    n_fft = wh%ndata
-    allocate(fft_data(wh%ncom, n_fft))
-    allocate(freq(n_fft/2))
-    allocate(amplitude(wh%ncom, n_fft/2))
+    ! Find next power of 2 for FFT
+    n_fft = 2 ** ceiling(log(real(wh%ndata)) / log(2.0))
+    n_freq = n_fft / 2 + 1
+    
+    allocate(fft_data(n_fft))
+    allocate(freq(n_freq))
+    allocate(amplitude(wh%ncom, n_freq))
     
     ! Calculate frequency array
-    do i = 1, n_fft/2
+    do i = 1, n_freq
         freq(i) = (i-1) * (1.0/(wh%dt * n_fft))
     end do
     
-    ! Perform FFT for each component
+    ! Initialize PGPLOT
+    call pgopen('../plot/waveform_fft.ps/VCPS')
+    call pgsubp(2, 3)  ! 2 rows, 3 columns
+    
+    ! Process and plot each component
     do i = 1, wh%ncom
-        ! Copy data to complex array
-        do j = 1, n_fft
-            fft_data(i,j) = cmplx(wd(i,j), 0.0)
+        ! Copy data to complex array and pad with zeros
+        fft_data = cmplx(0.0, 0.0)
+        do j = 1, wh%ndata
+            fft_data(j) = cmplx(wd(i,j), 0.0)
         end do
         
-        ! Perform FFT
-        call fft(fft_data(i,:), n_fft, 1)
+        ! Perform FFT using built-in Fortran function
+        fft_data = fft_data / sqrt(real(n_fft))  ! Normalize
+        call fft(fft_data)
         
         ! Calculate amplitude spectrum
-        do j = 1, n_fft/2
-            amplitude(i,j) = sqrt(real(fft_data(i,j))**2 + aimag(fft_data(i,j))**2)
+        do j = 1, n_freq
+            amplitude(i,j) = sqrt(real(fft_data(j))**2 + aimag(fft_data(j))**2)
             ! Convert to dB
             if (amplitude(i,j) > 0.0) then
                 amplitude(i,j) = 20.0 * log10(amplitude(i,j))
@@ -89,32 +98,24 @@ program PlotWaveformFFT
                 amplitude(i,j) = -200.0  ! Set minimum dB value
             end if
         end do
-    end do
-    
-    ! Initialize PGPLOT
-    call pgopen('../plot/waveform_fft.ps/VCPS')
-    call pgsubp(2, 3)  ! 2 rows, 3 columns
-    
-    ! Plot time domain waveforms
-    do i = 1, wh%ncom
+        
+        ! Plot time domain
         call pgsci(1)
         min_E = minval(wd(i,:))
         max_E = maxval(wd(i,:))
         call pgenv(minval(T), maxval(T), min_E, max_E, 0, 0)
         call pglab('Time (sec)', 'Amplitude', 'Waveform E'//char(i+48))
         call pgsci(i+1)
-        call pgline(n_fft, T, wd(i,:))
-    end do
-    
-    ! Plot frequency domain spectra
-    do i = 1, wh%ncom
+        call pgline(wh%ndata, T, wd(i,:))
+        
+        ! Plot frequency domain
         call pgsci(1)
         min_A = minval(amplitude(i,:))
         max_A = maxval(amplitude(i,:))
-        call pgenv(0.0, maxval(freq), min_A, max_A, 0, 0)
+        call pgenv(0.0, min(50.0, maxval(freq)), min_A, max_A, 0, 0)  ! Limit to 50 Hz
         call pglab('Frequency (Hz)', 'Amplitude (dB)', 'Spectrum E'//char(i+48))
         call pgsci(i+1)
-        call pgline(n_fft/2, freq, amplitude(i,:))
+        call pgline(n_freq, freq, amplitude(i,:))
     end do
     
     call pgclos()
@@ -123,55 +124,34 @@ program PlotWaveformFFT
     deallocate(wd, T, fft_data, freq, amplitude)
 
 contains
-    ! FFT subroutine (Cooley-Tukey algorithm)
-    subroutine fft(x, n, direction)
+    ! FFT subroutine using recursive algorithm
+    recursive subroutine fft(x)
         complex, intent(inout) :: x(:)
-        integer, intent(in) :: n, direction
-        complex :: temp
-        integer :: i, j, k, m, lstep
-        real :: angle
-        real, parameter :: pi = 3.14159265359
+        complex, allocatable :: even(:), odd(:)
+        complex :: w
+        integer :: n, k
         
-        ! Bit-reverse ordering
-        j = 1
-        do i = 1, n-1
-            if (i < j) then
-                temp = x(j)
-                x(j) = x(i)
-                x(i) = temp
-            end if
-            m = n/2
-            do while (j > m .and. m > 0)
-                j = j - m
-                m = m/2
-            end do
-            j = j + m
+        n = size(x)
+        if (n <= 1) return
+        
+        allocate(even(n/2), odd(n/2))
+        
+        ! Split into even and odd
+        even = x(1:n:2)
+        odd = x(2:n:2)
+        
+        ! Recursive FFT
+        call fft(even)
+        call fft(odd)
+        
+        ! Combine
+        do k = 1, n/2
+            w = cmplx(cos(-2.0*pi*(k-1)/n), sin(-2.0*pi*(k-1)/n))
+            x(k) = even(k) + w * odd(k)
+            x(k + n/2) = even(k) - w * odd(k)
         end do
         
-        ! Compute FFT
-        do lstep = 1, log2(real(n))
-            m = 2**lstep
-            do k = 1, n, m
-                do i = k, k+m/2-1
-                    angle = -2.0 * pi * direction * (i-k) / m
-                    temp = x(i+m/2) * cmplx(cos(angle), sin(angle))
-                    x(i+m/2) = x(i) - temp
-                    x(i) = x(i) + temp
-                end do
-            end do
-        end do
-        
-        ! Scale if inverse FFT
-        if (direction == -1) then
-            x = x / n
-        end if
+        deallocate(even, odd)
     end subroutine fft
-    
-    ! Function to compute log2
-    function log2(x)
-        real, intent(in) :: x
-        integer :: log2
-        log2 = int(log(x)/log(2.0))
-    end function log2
     
 end program PlotWaveformFFT
